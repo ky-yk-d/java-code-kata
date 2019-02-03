b20a9048777a41b308e69eec7c9f98dfed6b7271
ky-yk-d <key0of7one0s5life@gmail.com>
2019-02-04 01:01:48 +0900

    実験コードを追加

--- src/test/java/stream/sample/SampleTest.java
+++ src/test/java/stream/sample/SampleTest.java
@@ -1,19 +1,74 @@
 package stream.sample;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import org.junit.Assert;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 class SampleTest {
 
-	@Test
-	void test() {
-		Stream<Object> st = Stream.of((Object)null);
-		System.out.println(st);
-		st.forEach(System.out::println); // nullが出力される
- 		Stream<Object> nst = Stream.ofNullable((Object)null);
-		System.out.println(nst);
-		nst.forEach(System.out::println);  // 要素がないので出力されない
+	@Nested
+	class filterの仕様の確認{
+	
+		@Test
+		void Optionalのfilterが空のOptionalを返す() {
+			Optional<Integer> opt = Optional.of(3).filter(e -> e % 2 == 0);
+			assertTrue(opt.isEmpty());
+		}
+		
+		@Test
+		void Streamのfilterが空のStreamを返す() {
+			List<Integer> list = Stream.of(1,3)
+					.filter(e -> e % 2 == 0)
+					.collect(Collectors.toList());
+				assertEquals(0, list.size());
+		}
 	}
+	
+	@Nested
+	class ofとofNullableの仕様の確認{
 
+		@Test
+		void Optionalのof() {
+			Optional<Object> opt = null;
+			try {
+				opt = Optional.of(null);
+			} catch (Exception e) {
+				assertEquals(NullPointerException.class, e.getClass());
+			}
+			assertNull(opt);
+		}
+		
+		@Test
+		void OptionalのofNullable() {
+			Optional<Object> opt = Optional.ofNullable(null);
+			assertTrue(opt.isEmpty());
+			opt = Optional.ofNullable(42);
+			assertEquals(42, opt.get());
+		}
+		
+		@Test
+		void Streamのof() {
+			List<Object> list = Stream.of((Object)null)
+									.collect(Collectors.toList());
+			assertEquals(1, list.size());
+		}
+		
+		@Test
+		void StreamのofNullable() {
+			List<Object> list = Stream.ofNullable((Object)null)
+									.collect(Collectors.toList());
+			assertTrue(list.isEmpty());
+		}
+	}
 }
