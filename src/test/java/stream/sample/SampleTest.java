237ba2af8008a650b5eebd006d63994e4c1ad740
ky-yk-d <key0of7one0s5life@gmail.com>
2019-02-03 22:23:37 +0900

    Stream.ofNullable()の挙動を確認

--- src/test/java/stream/sample/SampleTest.java
+++ src/test/java/stream/sample/SampleTest.java
@@ -10,8 +10,10 @@ class SampleTest {
 	void test() {
 		Stream<Object> st = Stream.of((Object)null);
 		System.out.println(st);
-		st.forEach(System.out::println);
-//		Stream<Object> nst = Stream.ofNullable((Object)null);
+		st.forEach(System.out::println); // nullが出力される
+ 		Stream<Object> nst = Stream.ofNullable((Object)null);
+		System.out.println(nst);
+		nst.forEach(System.out::println);  // 要素がないので出力されない
 	}
 
 }
