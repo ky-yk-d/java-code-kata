bb7be7ec376cf6729855f3e0ae6b87e69a02ea89
ky-yk-d <key0of7one0s5life@gmail.com>
2019-02-07 22:55:25 +0900

    パラメタライズドテストの実験を追加

--- src/test/java/stream/sample/SampleTest.java
+++ src/test/java/stream/sample/SampleTest.java
@@ -1,31 +1,31 @@
 package stream.sample;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
+import java.time.YearMonth;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 import java.util.List;
-import java.util.NoSuchElementException;
 import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-import org.junit.Assert;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class SampleTest {
 
 	@Nested
 	class filterの仕様の確認{
-	
+
 		@Test
 		void Optionalのfilterが空のOptionalを返す() {
 			Optional<Integer> opt = Optional.of(3).filter(e -> e % 2 == 0);
 			assertTrue(opt.isEmpty());
 		}
-		
+
 		@Test
 		void Streamのfilterが空のStreamを返す() {
 			List<Integer> list = Stream.of(1,3)
@@ -34,7 +34,7 @@ class SampleTest {
 				assertEquals(0, list.size());
 		}
 	}
-	
+
 	@Nested
 	class ofとofNullableの仕様の確認{
 
@@ -48,7 +48,7 @@ class SampleTest {
 			}
 			assertNull(opt);
 		}
-		
+
 		@Test
 		void OptionalのofNullable() {
 			Optional<Object> opt = Optional.ofNullable(null);
@@ -56,14 +56,14 @@ class SampleTest {
 			opt = Optional.ofNullable(42);
 			assertEquals(42, opt.get());
 		}
-		
+
 		@Test
 		void Streamのof() {
 			List<Object> list = Stream.of((Object)null)
 									.collect(Collectors.toList());
 			assertEquals(1, list.size());
 		}
-		
+
 		@Test
 		void StreamのofNullable() {
 			List<Object> list = Stream.ofNullable((Object)null)
@@ -71,4 +71,23 @@ class SampleTest {
 			assertTrue(list.isEmpty());
 		}
 	}
+
+	@Nested
+	class JUnit5の練習{
+		@ParameterizedTest
+		@ValueSource(strings = {"201801","199912"})
+		void 年月の表現として有効な文字列(String str) {
+			DateTimeFormatter yyyymm = DateTimeFormatter.ofPattern("yyyyMM");
+			YearMonth yearMonth = YearMonth.parse(str, yyyymm);
+			assertNotNull(yearMonth);
+		}
+
+		@ParameterizedTest
+		@ValueSource(strings = {"20181", "20180A", "-010012", "200012011"})
+		void 年月の表現として無効な文字列(String str) {
+			DateTimeFormatter yyyymm = DateTimeFormatter.ofPattern("yyyyMM");
+			assertThrows(DateTimeParseException.class, ()->YearMonth.parse(str, yyyymm));
+		}
+	}
+
 }
